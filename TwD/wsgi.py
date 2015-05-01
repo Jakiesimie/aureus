661bd29d6c836aafecfdeaff011dcf363aed5a21
jakiesimie <luciusv@o2.pl>
2015-05-01 16:50:04 +0200

    myblog #2

--- TwD/wsgi.py
+++ TwD/wsgi.py
@@ -7,11 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+#~ import os
 from django.core.wsgi import get_wsgi_application
-#~ from whitenoise.django import DjangoWhiteNoise
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TwD.settings")
+#~ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TwD.settings")
 
 application = get_wsgi_application()
-#~ application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(application)
