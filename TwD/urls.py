79093da93e931556a73e730aeeae78f0605bbc01
jakiesimie <luciusv@o2.pl>
2015-05-01 16:42:55 +0200

    myblog #1

---
+++ TwD/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^rango/', include('rango.urls')),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    url(r'', include('blog.urls')),
+]
